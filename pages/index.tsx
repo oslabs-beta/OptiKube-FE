307bdae96efebf8aa318fcc2df3b70d9e5e9a2f7
Andy Guajardo <andymatt1225@gmail.com>
2024-04-18 19:39:35 -0700

    changes to index.tsx and tailwind.config

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 // "use client";
 import React from "react";
-import { Boxes } from "../components/Background-Boxes";
+import { Boxes } from "../components/background-boxes";
 import { cn } from "../utils/cn";
 import Link from 'next/link';
 
