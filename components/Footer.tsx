fc9d0c96da341e26441d97060f6434b184928635
Cheryl Lee <yli.yanchen@gmail.com>
2024-04-19 15:17:30 -0700

    footer works

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,12 +1,66 @@
-// components/NavBar.tsx
-
-// **DO NOT USE, CODEBLOCK BREAKS APPLICAITON**
-import React from "react";
+import React, { useState } from "react";
 import Link from "next/link";
 
 const Footer: React.FC = () => {
+  const [menu, setMenu] = useState<string>("");
+
   return (
-    <p> this is footer </p>
+    <div className="w-full h-30 flex flex-col items-center justify-center bg-thdwhite bg-opacity-15">
+      <div className="block text-priblue text-start no-underline leading-10 items-center align-middle gap-5 text-sm mx-4 font-bold mt-6 mb-0">
+        Quick Links
+      </div>
+      <div className="flex flex-row">
+        <ul className="navbar flex flex-row items-center text-center">
+          <li
+            className="block text-priblue text-start no-underline font-normal leading-10 items-center align-middle gap-5 text-sm mx-4"
+            onClick={() => setMenu("User")}
+          >
+            <Link href={"/userDoshboard"}>
+              <p style={{ textDecoration: "none" }}>
+                • User
+                {menu === "User" ? <hr className="bg-secwhite" /> : <></>}
+              </p>
+            </Link>
+          </li>
+
+          <li
+            className="block text-priblue text-start no-underline font-normal leading-10 items-center align-middle gap-5 text-sm mx-4"
+            onClick={() => setMenu("Matrixes")}
+          >
+            <Link href={"/MatricsDashboard"}>
+              <p style={{ textDecoration: "none" }}>
+                • Matrixes {menu === "Matrixes" ? <hr /> : <></>}
+              </p>
+            </Link>
+          </li>
+
+          <li
+            className="block text-priblue text-start no-underline font-normal leading-10 items-center align-middle gap-5 text-sm mx-4"
+            onClick={() => setMenu("HPA")}
+          >
+            <Link href={"/HPADashboard"}>
+              <p style={{ textDecoration: "none" }}>
+                • HPA{menu === "HPA" ? <hr /> : <></>}
+              </p>
+            </Link>
+          </li>
+
+          <li
+            className="block text-priblue text-start no-underline font-normal leading-10 items-center align-middle gap-5 text-sm mx-4"
+            onClick={() => setMenu("GitHub")}
+          >
+            <Link href={"https://github.com/oslabs-beta/Kubernetes-OSP"}>
+              <p style={{ textDecoration: "none" }}>
+                • GitHub {menu === "GitHub" ? <hr /> : <></>}
+              </p>
+            </Link>
+          </li>
+        </ul>
+      </div>
+      <div className="block text-priblue text-start no-underline font-normal leading-10 items-center align-middle gap-5 text-sm mx-4 font-sm mt-2 mb-10">
+        ©2023 - 2024 Optikube. All rights reserved.
+      </div>
+    </div>
   );
 };
 
--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -64,7 +64,7 @@ const NavBar = () => {
 
           <NavigationMenu.Item>
             <NavigationMenu.Trigger className="text-violet11 hover:bg-violet3 focus:shadow-violet7 group flex select-none items-center justify-between gap-[2px] rounded-[4px] px-4 py-2 text-[15px] font-medium leading-none outline-none focus:shadow-[0_0_0_2px]">
-              Cost{" "}
+              HPA{" "}
               <CaretDownIcon
                 className="text-violet10 relative top-[1px] transition-transform duration-[250] ease-in group-data-[state=open]:-rotate-180"
                 aria-hidden
