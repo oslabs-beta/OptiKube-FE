dd5f2c8d7e4cba727dae5315a7eb7eb55f0e026f
Andy Guajardo <andymatt1225@gmail.com>
2024-04-22 21:07:54 -0700

    HPA Form component - HPA form as component and rendered on test pages - Minor change to Landing page button

--- pages/TestPages/testPage2.tsx
+++ pages/TestPages/testPage2.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 import { BentoGridDemo, BentoGridThirdDemo, BentoGridSecondDemo } from '../../components/bento-2x2';
 
 import NavBar from "../../components/NavBar";
-
+import HPAForm from 'components/HPA-Form';
 
 
 export default function testPage() {
@@ -14,6 +14,7 @@ export default function testPage() {
 
         <div>
             <NavBar />
+            <HPAForm />
             <div className='flex flex-col items-center justify-center'>
                 <Link href="/" passHref>
                     <button className="mt-20 text-white bg-gradient-to-r from-teal-400 via-teal-500 to-teal-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-teal-300 dark:focus:ring-teal-800 shadow-lg shadow-teal-500/50 dark:shadow-lg dark:shadow-teal-800/80 font-medium rounded-lg text-sm h-20 px-10 py-2.5 text-center me-2 mb-2 ">Back home</button>
@@ -21,7 +22,7 @@ export default function testPage() {
 
             </div>
             {/* <BentoGridDemo /> */}
-            <BentoGridSecondDemo />
+            {/* <BentoGridSecondDemo /> */}
             {/* <BentoGridThirdDemo /> */}
 
         </div>
