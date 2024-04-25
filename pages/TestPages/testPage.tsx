73536b3ec742cd9106f402b9adb4f6d00e9b0ae0
Andy Guajardo <andymatt1225@gmail.com>
2024-04-24 17:35:38 -0700

    minor corrects to test pages

--- pages/TestPages/testPage.tsx
+++ pages/TestPages/testPage.tsx
@@ -15,7 +15,7 @@ export default function testPage() {
                     </p>
 
                     <Link href="/" passHref>
-                        <button className="mt-20 text-white bg-gradient-to-r from-teal-400 via-teal-500 to-teal-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-teal-300 dark:focus:ring-teal-800 shadow-lg shadow-teal-500/50 dark:shadow-lg dark:shadow-teal-800/80 font-medium rounded-lg text-sm h-20 px-10 py-2.5 text-center me-2 mb-2 ">Back home</button>
+                        <button className="mt-20 text-black bg-gradient-to-r from-teal-400 via-teal-500 to-teal-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-teal-300 dark:focus:ring-teal-800 shadow-lg shadow-teal-500/50 dark:shadow-lg dark:shadow-teal-800/80 font-medium rounded-lg text-sm h-20 px-10 py-2.5 text-center me-2 mb-2 ">Back home</button>
                     </Link>
                 </div>
             </BackgroundGradientAnimation>
