71c4857e66b99c6902592462814595cd56fba7ce
Cheryl Lee <yli.yanchen@gmail.com>
2024-05-11 13:50:01 -0700

    TrendsDashboard setup table

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,28 +1,27 @@
-import React from "react";
-import * as NavigationMenu from "@radix-ui/react-navigation-menu";
-import classNames from "classnames";
-import { CaretDownIcon } from "@radix-ui/react-icons";
-import { cn } from "../public/utils/shadcnUI/cn";
+import React from 'react';
+import * as NavigationMenu from '@radix-ui/react-navigation-menu';
+import classNames from 'classnames';
+import { CaretDownIcon } from '@radix-ui/react-icons';
+import { cn } from '../public/utils/shadcnUI/cn';
 
-import Link from "next/link";
-
+import Link from 'next/link';
 
 const NavBar = () => {
   return (
-    <div className="h-12 bg-blue-950 m-0">
-      <NavigationMenu.Root className="relative z-50 flex w-screen justify-center">
-        <NavigationMenu.List className="center shadow-blackA4 m-1 flex list-none rounded-[6px] bg-blue-950 p-1 px-8">
+    <div className='h-12 bg-blue-950 m-0'>
+      <NavigationMenu.Root className='relative z-50 flex w-screen justify-center'>
+        <NavigationMenu.List className='center shadow-blackA4 m-1 flex list-none rounded-[6px] bg-blue-950 p-1 px-8'>
           <NavigationMenu.Item>
-            <NavigationMenu.Trigger className="text-zinc-200 hover:bg-violet3 focus:shadow-violet7 group flex select-none items-center justify-between gap-[2px] rounded-[4px] px-4 py-2 text-[15px] font-medium leading-none outline-none focus:shadow-[0_0_0_2px]">
-              User{" "}
+            <NavigationMenu.Trigger className='text-zinc-200 hover:bg-violet3 focus:shadow-violet7 group flex select-none items-center justify-between gap-[2px] rounded-[4px] px-4 py-2 text-[15px] font-medium leading-none outline-none focus:shadow-[0_0_0_2px]'>
+              User{' '}
               <CaretDownIcon
-                className="text-zinc-200 relative top-[1px] transition-transform duration-[250] ease-in group-data-[state=open]:-rotate-180"
+                className='text-zinc-200 relative top-[1px] transition-transform duration-[250] ease-in group-data-[state=open]:-rotate-180'
                 aria-hidden
               />
             </NavigationMenu.Trigger>
-            <NavigationMenu.Content className="bg-slate-200 data-[motion=from-start]:animate-enterFromLeft data-[motion=from-end]:animate-enterFromRight data-[motion=to-start]:animate-exitToLeft data-[motion=to-end]:animate-exitToRight absolute top-0 left-0 w-auto sm:w-auto">
-              <ul className="one m-0 grid list-none gap-x-[10px] p-[22px] sm:w-[500px]">
-                <ListItem href="https://stitches.dev/" title="Overview">
+            <NavigationMenu.Content className='bg-slate-200 data-[motion=from-start]:animate-enterFromLeft data-[motion=from-end]:animate-enterFromRight data-[motion=to-start]:animate-exitToLeft data-[motion=to-end]:animate-exitToRight absolute top-0 left-0 w-auto sm:w-auto'>
+              <ul className='one m-0 grid list-none gap-x-[10px] p-[22px] sm:w-[500px]'>
+                <ListItem href='https://stitches.dev/' title='Overview'>
                   Get high-level information regarding your cluster.
                 </ListItem>
               </ul>
@@ -30,41 +29,39 @@ const NavBar = () => {
           </NavigationMenu.Item>
 
           <NavigationMenu.Item>
-            <NavigationMenu.Trigger className="text-zinc-200 hover:bg-violet3 focus:shadow-violet7 group flex select-none items-center justify-between gap-[2px] rounded-[4px] px-4 py-2 text-[15px] font-medium leading-none outline-none focus:shadow-[0_0_0_2px]">
-              Dashboards{" "}
+            <NavigationMenu.Trigger className='text-zinc-200 hover:bg-violet3 focus:shadow-violet7 group flex select-none items-center justify-between gap-[2px] rounded-[4px] px-4 py-2 text-[15px] font-medium leading-none outline-none focus:shadow-[0_0_0_2px]'>
+              Dashboards{' '}
               <CaretDownIcon
-                className="text-zinc-200 relative top-[1px] transition-transform duration-[250] ease-in group-data-[state=open]:-rotate-180"
+                className='text-zinc-200 relative top-[1px] transition-transform duration-[250] ease-in group-data-[state=open]:-rotate-180'
                 aria-hidden
               />
             </NavigationMenu.Trigger>
-            <NavigationMenu.Content className="absolute top-0 left-0 w-full sm:w-auto bg-slate-200">
-              <ul className="m-0 grid list-none gap-x-[10px] p-[22px] sm:w-[500px] sm:grid-flow-col sm:grid-rows-3">
-                <Link href="/Dashboards/MetricsDashboard" passHref>
+            <NavigationMenu.Content className='absolute top-0 left-0 w-full sm:w-auto bg-slate-200'>
+              <ul className='m-0 grid list-none gap-x-[10px] p-[22px] sm:w-[500px] sm:grid-flow-col sm:grid-rows-3'>
+                <Link href='/Dashboards/MetricsDashboard' passHref>
                   <ListItem
-                    title="Metrics Dashboard"
-                    href="/primitives/docs/overview/introduction"
+                    title='Metrics Dashboard'
+                    href='/primitives/docs/overview/introduction'
                   >
                     View Deployment Metrics and Visualizations
                   </ListItem>
                 </Link>
 
-
-                <Link href="/Dashboards/CostDashboard" passHref >
+                <Link href='/Dashboards/CostDashboard' passHref>
                   <ListItem
-                    title="Cost Dashboard"
-                    href="/primitives/docs/overview/getting-started"
+                    title='Cost Dashboard'
+                    href='/primitives/docs/overview/getting-started'
                   >
                     View Deployment Cost Metrics and Visualizations
                   </ListItem>
                 </Link>
 
-
-                <Link href="/Dashboards/HPADashboard" passHref>
+                <Link href='/Dashboards/TrendsDashboard' passHref>
                   <ListItem
-                    title="HPA Dashboard"
-                    href="/primitives/docs/guides/styling"
+                    title='Trends Dashboard'
+                    href='/primitives/docs/guides/styling'
                   >
-                    View HPA metrics and customize your HPA here
+                    Trends Visualizations
                   </ListItem>
                 </Link>
               </ul>
@@ -72,16 +69,16 @@ const NavBar = () => {
           </NavigationMenu.Item>
 
           <NavigationMenu.Item>
-            <NavigationMenu.Trigger className="text-zinc-200 hover:bg-violet3 focus:shadow-violet7 group flex select-none items-center justify-between gap-[2px] rounded-[4px] px-4 py-2 text-[15px] font-medium leading-none outline-none focus:shadow-[0_0_0_2px]">
-              HPA{" "}
+            <NavigationMenu.Trigger className='text-zinc-200 hover:bg-violet3 focus:shadow-violet7 group flex select-none items-center justify-between gap-[2px] rounded-[4px] px-4 py-2 text-[15px] font-medium leading-none outline-none focus:shadow-[0_0_0_2px]'>
+              HPA{' '}
               <CaretDownIcon
-                className="text-zinc-200 relative top-[1px] transition-transform duration-[250] ease-in group-data-[state=open]:-rotate-180"
+                className='text-zinc-200 relative top-[1px] transition-transform duration-[250] ease-in group-data-[state=open]:-rotate-180'
                 aria-hidden
               />
             </NavigationMenu.Trigger>
-            <NavigationMenu.Content className="bg-slate-200 data-[motion=from-start]:animate-enterFromLeft data-[motion=from-end]:animate-enterFromRight data-[motion=to-start]:animate-exitToLeft data-[motion=to-end]:animate-exitToRight absolute top-0 left-0 w-full sm:w-auto">
-              <ul className="one m-0 grid list-none gap-x-[10px] p-[22px] sm:w-[500px]">
-                <ListItem href="https://stitches.dev/" title="Overview">
+            <NavigationMenu.Content className='bg-slate-200 data-[motion=from-start]:animate-enterFromLeft data-[motion=from-end]:animate-enterFromRight data-[motion=to-start]:animate-exitToLeft data-[motion=to-end]:animate-exitToRight absolute top-0 left-0 w-full sm:w-auto'>
+              <ul className='one m-0 grid list-none gap-x-[10px] p-[22px] sm:w-[500px]'>
+                <ListItem href='https://stitches.dev/' title='Overview'>
                   Dive into the cost analysis
                 </ListItem>
               </ul>
@@ -90,30 +87,29 @@ const NavBar = () => {
 
           <NavigationMenu.Item>
             <NavigationMenu.Link
-              className="text-zinc-200 hover:bg-violet3 focus:shadow-violet7 block select-none rounded-[4px] px-3 py-2 text-[15px] font-medium leading-none no-underline outline-none focus:shadow-[0_0_0_2px]"
-              href="https://github.com/oslabs-beta/Kubernetes-OSP"
+              className='text-zinc-200 hover:bg-violet3 focus:shadow-violet7 block select-none rounded-[4px] px-3 py-2 text-[15px] font-medium leading-none no-underline outline-none focus:shadow-[0_0_0_2px]'
+              href='https://github.com/oslabs-beta/Kubernetes-OSP'
             >
               Github
             </NavigationMenu.Link>
           </NavigationMenu.Item>
 
-          <NavigationMenu.Indicator className="data-[state=visible]:animate-fadeIn data-[state=hidden]:animate-fadeOut top-full z-[1] flex h-[10px] items-end justify-center overflow-hidden transition-[width,transform_250ms_ease]">
-            <div className="relative top-[70%] h-[10px] w-[10px] rotate-[45deg] rounded-tl-[2px] bg-white" />
+          <NavigationMenu.Indicator className='data-[state=visible]:animate-fadeIn data-[state=hidden]:animate-fadeOut top-full z-[1] flex h-[10px] items-end justify-center overflow-hidden transition-[width,transform_250ms_ease]'>
+            <div className='relative top-[70%] h-[10px] w-[10px] rotate-[45deg] rounded-tl-[2px] bg-white' />
           </NavigationMenu.Indicator>
         </NavigationMenu.List>
 
-        <div className="perspective-[2000px] absolute top-full left-0 flex w-full justify-center">
-          <NavigationMenu.Viewport className="data-[state=open]:animate-scaleIn data-[state=closed]:animate-scaleOut relative mt-[10px] h-[var(--radix-navigation-menu-viewport-height)] w-full origin-[top_center] overflow-hidden rounded-[6px] bg-white transition-[width,_height] duration-300 sm:w-[var(--radix-navigation-menu-viewport-width)]" />
+        <div className='perspective-[2000px] absolute top-full left-0 flex w-full justify-center'>
+          <NavigationMenu.Viewport className='data-[state=open]:animate-scaleIn data-[state=closed]:animate-scaleOut relative mt-[10px] h-[var(--radix-navigation-menu-viewport-height)] w-full origin-[top_center] overflow-hidden rounded-[6px] bg-white transition-[width,_height] duration-300 sm:w-[var(--radix-navigation-menu-viewport-width)]' />
         </div>
       </NavigationMenu.Root>
     </div>
   );
 };
 
-
 const ListItem = React.forwardRef<
-  React.ElementRef<"a">,
-  React.ComponentPropsWithoutRef<"a">
+  React.ElementRef<'a'>,
+  React.ComponentPropsWithoutRef<'a'>
 >(({ className, title, children, ...props }, ref) => {
   return (
     <li>
@@ -121,13 +117,13 @@ const ListItem = React.forwardRef<
         <a
           ref={ref}
           className={cn(
-            "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
+            'block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground',
             className
           )}
           {...props}
         >
-          <div className="text-sm font-medium leading-none">{title}</div>
-          <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
+          <div className='text-sm font-medium leading-none'>{title}</div>
+          <p className='line-clamp-2 text-sm leading-snug text-muted-foreground'>
             {children}
           </p>
         </a>
@@ -135,5 +131,5 @@ const ListItem = React.forwardRef<
     </li>
   );
 });
-ListItem.displayName = "ListItem";
+ListItem.displayName = 'ListItem';
 export default NavBar;
---
+++ pages/Dashboards/TrendsDashboard.tsx
@@ -0,0 +1,121 @@
+// @ts-ignore
+
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import {
+  Table,
+  TableHeader,
+  TableBody,
+  TableColumn,
+  TableRow,
+  TableCell,
+} from '@nextui-org/table';
+
+import NavBar from '../../components/NavBar';
+import Footer from '../../components/Footer';
+
+const TrendsDashboard = () => {
+  const [kubecost, setKubecost] = useState({});
+
+  const rows = ['CPU', 'RAW', 'LoadBalance', 'TotalCost', 'Saving'];
+
+  const columns = [
+    '__idle__',
+    'kube-system',
+    'kubecost',
+    'dummy-nginx',
+    'optikube',
+    'keda',
+    'dummy-php',
+    'gmp-system',
+    '__unmounted__',
+  ];
+
+  const fetchTrend = async (name) => {
+    try {
+      const response = await axios.get(
+        `http://localhost:9090/model/allocation/trends?window=1d&aggregate=namespace&names=${name}&accumulate=true`
+      );
+
+      const totalCostValue =
+        Number(
+          response.data.data.sets[0]?.allocationTrends?.kubecost?.trends?.costs
+            ?.totalCost?.relativeChange?.value
+        ) * 100;
+      console.log('>>> totalCostValue', totalCostValue);
+      return totalCostValue;
+    } catch (error) {
+      console.error('Error fetching trend:', error);
+      return 0; // Return a default value if fetching fails
+    }
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const results = await Promise.all(
+          columns.map((column) => fetchTrend(column)) // Use column instead of row
+        );
+        const kubecostData = {};
+        results.forEach((totalCost, index) => {
+          kubecostData[columns[index]] = totalCost;
+        });
+        setKubecost(kubecostData);
+        console.log('kubecost:', kubecostData);
+      } catch (error) {
+        console.error('Error fetching trends:', error);
+      }
+    };
+
+    fetchData();
+  }, []); // Empty dependency array for useEffect to run once on mount
+
+  return (
+    <div className='flex flex-col'>
+      <NavBar />
+      <div className='flex flex-row justify-left mt-14 mb-0 ml-8 mx-3'>
+        <h2 className='font-bold text-2xl'>
+          Cumulative cost for last 7 days by namespace
+        </h2>
+      </div>
+      <div className='flex flex-row  bg-slate-200 my-6 mx-3'>
+        <Table isStriped aria-label='Example static collection table'>
+          <TableHeader className='font-bold'>
+            <TableColumn>Namespace</TableColumn>
+            <TableColumn>CPU</TableColumn>
+            <TableColumn>GPU</TableColumn>
+            <TableColumn>RAW</TableColumn>
+            <TableColumn>Network</TableColumn>
+            <TableColumn>LoadBalance</TableColumn>
+            <TableColumn>Efficiency</TableColumn>
+          </TableHeader>
+          <TableBody>
+            {/* <TableRow key='1'>
+              <TableCell>Tony Reichert</TableCell>
+              <TableCell>CEO</TableCell>
+              <TableCell>Active</TableCell>
+            </TableRow>
+            <TableRow key='2'>
+              <TableCell>Zoey Lang</TableCell>
+              <TableCell>Technical Lead</TableCell>
+              <TableCell>Paused</TableCell>
+            </TableRow>
+            <TableRow key='3'>
+              <TableCell>Jane Fisher</TableCell>
+              <TableCell>Senior Developer</TableCell>
+              <TableCell>Active</TableCell>
+            </TableRow>
+            <TableRow key='4'>
+              <TableCell>William Howard</TableCell>
+              <TableCell>Community Manager</TableCell>
+              <TableCell>Vacation</TableCell>
+            </TableRow> */}
+          </TableBody>
+        </Table>
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default TrendsDashboard;
