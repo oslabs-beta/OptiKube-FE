fb3e1ee7cc21e74077f945372003a496080145a9
Josh <josh.r.bajarias@gmail.com>
2024-05-14 21:32:16 -0400

    Code: fixed rendering issues with dynamic charts and moving components.

---
+++ components/AreaChart.tsx
@@ -0,0 +1,47 @@
+// @ts-ignore
+import { useEffect, useRef } from 'react';
+import dynamic from 'next/dynamic';
+
+const DynamicApexCharts = dynamic(() => import('react-apexcharts'), {
+  ssr: false,
+});
+
+const AreaChart = ({ data, xName, yName }) => {
+  const chartRef = useRef(null);
+
+  useEffect(() => {
+    if (chartRef.current && data && data.length > 0) {
+      import('apexcharts').then(({ default: ApexCharts }) => {
+        const metrixesKeys = data.map((item) => item.timeStart);
+        const metrixesValues = data.map((item) => item.totalCost);
+
+        const options = {
+          series: [{ name: yName, data: metrixesValues }],
+          chart: { height: 350, width: 600, type: 'area' },
+          xaxis: { categories: metrixesKeys },
+          plotOptions: {
+            bar: {
+              dataLabels: { position: 'top' }, // Show labels at the top of each bar
+            },
+          },
+          legend: {
+            show: true, // Show legend
+            position: 'bottom', // Position legend at the bottom
+          },
+          dataLabels: { enabled: false },
+        };
+
+        const chart = new ApexCharts(chartRef.current, options);
+        chart.render();
+
+        return () => {
+          chart.destroy();
+        };
+      });
+    }
+  }, [data, xName, yName]);
+
+  return <div className='w-1/2 p-4' ref={chartRef} />;
+};
+
+export default AreaChart;
--- components/Infinite-Moving-Cards.tsx
+++ components/Infinite-Moving-Cards.tsx
@@ -1,118 +1,304 @@
-"use client";
+// "use client";
+
+// import { cn } from "../utils/cn";
+// import React, { useEffect, useState } from "react";
+
+// export const InfiniteMovingCards = ({
+//   items,
+//   direction = "left",
+//   speed = "fast",
+//   pauseOnHover = true,
+//   className,
+// }: {
+//   items: {
+//     quote: string;
+//     name: string;
+//     title: string;
+//   }[];
+//   direction?: "left" | "right";
+//   speed?: "fast" | "normal" | "slow";
+//   pauseOnHover?: boolean;
+//   className?: string;
+// }) => {
+//   const containerRef = React.useRef<HTMLDivElement>(null);
+//   const scrollerRef = React.useRef<HTMLUListElement>(null);
+
+//   useEffect(() => {
+//     addAnimation();
+//   }, []);
+//   const [start, setStart] = useState(false);
+//   function addAnimation() {
+//     if (containerRef.current && scrollerRef.current) {
+//       const scrollerContent = Array.from(scrollerRef.current.children);
+
+//       scrollerContent.forEach((item) => {
+//         const duplicatedItem = item.cloneNode(true);
+//         if (scrollerRef.current) {
+//           scrollerRef.current.appendChild(duplicatedItem);
+//         }
+//       });
+
+//       getDirection();
+//       getSpeed();
+//       setStart(true);
+//     }
+//   }
+//   const getDirection = () => {
+//     if (containerRef.current) {
+//       if (direction === "left") {
+//         containerRef.current.style.setProperty(
+//           "--animation-direction",
+//           "forwards"
+//         );
+//       } else {
+//         containerRef.current.style.setProperty(
+//           "--animation-direction",
+//           "reverse"
+//         );
+//       }
+//     }
+//   };
+//   const getSpeed = () => {
+//     if (containerRef.current) {
+//       if (speed === "fast") {
+//         containerRef.current.style.setProperty("--animation-duration", "20s");
+//       } else if (speed === "normal") {
+//         containerRef.current.style.setProperty("--animation-duration", "40s");
+//       } else {
+//         containerRef.current.style.setProperty("--animation-duration", "80s");
+//       }
+//     }
+//   };
+//   return (
+//     <div
+//       ref={containerRef}
+//       className={cn(
+//         "scroller relative z-20  max-w-7xl overflow-hidden  [mask-image:linear-gradient(to_right,transparent,white_20%,white_80%,transparent)]",
+//         className
+//       )}
+//     >
+//       <ul
+//         ref={scrollerRef}
+//         className={cn(
+//           " flex min-w-full shrink-0 gap-4 py-4 w-max flex-nowrap",
+//           start && "animate-scroll ",
+//           pauseOnHover && "hover:[animation-play-state:paused]"
+//         )}
+//       >
+//         {items.map((item, idx) => (
+//           <li
+//             className="w-[350px] max-w-full relative h-64 rounded-2xl border border-b-0 flex-shrink-0 border-slate-700 px-8 py-6 md:w-[450px]"
+//             style={{
+//               background:
+//                 "linear-gradient(180deg, var(--slate-800), var(--slate-900)",
+//             }}
+//             key={item.name}
+//           >
+//             <blockquote>
+//               <div
+//                 aria-hidden="true"
+//                 className="user-select-none -z-1 pointer-events-none absolute -left-0.5 -top-0.5 h-[calc(100%_+_4px)] w-[calc(100%_+_4px)]"
+//               ></div>
+//               <span className=" relative z-20 text-sm leading-[1.6] text-gray-100 font-normal">
+//                 {item.quote}
+//               </span>
+//               <div className="relative z-20 mt-6 flex flex-row items-center">
+//                 <span className="flex flex-col gap-1">
+//                   <span className=" text-sm leading-[1.6] text-gray-400 font-normal">
+//                     {item.name}
+//                   </span>
+//                   <span className=" text-sm leading-[1.6] text-gray-400 font-normal">
+//                     {item.title}
+//                   </span>
+//                 </span>
+//               </div>
+//             </blockquote>
+//           </li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// };
+
+// import { cn } from "../utils/cn";
+// import React, { useEffect, useState } from "react";
+// import DonutChart from './DonutChart';
+// import AreaChart from './AreaChart';
+
+// export const InfiniteMovingCards = ({
+//   items,
+//   direction = "left",
+//   speed = "fast",
+//   pauseOnHover = true,
+//   className,
+// }: {
+//   items: any[]; // Updated to any[] to accept different item types
+//   direction?: "left" | "right";
+//   speed?: "fast" | "normal" | "slow";
+//   pauseOnHover?: boolean;
+//   className?: string;
+// }) => {
+//   const containerRef = React.useRef<HTMLDivElement>(null);
+//   const scrollerRef = React.useRef<HTMLUListElement>(null);
+
+//   useEffect(() => {
+//     addAnimation();
+//   }, []);
+//   const [start, setStart] = useState(false);
+//   function addAnimation() {
+//     if (containerRef.current && scrollerRef.current) {
+//       const scrollerContent = Array.from(scrollerRef.current.children);
+
+//       scrollerContent.forEach((item) => {
+//         const duplicatedItem = item.cloneNode(true);
+//         scrollerRef.current.appendChild(duplicatedItem);
+//       });
+
+//       getDirection();
+//       getSpeed();
+//       setStart(true);
+//     }
+//   }
+//   const getDirection = () => {
+//     containerRef.current.style.setProperty(
+//       "--animation-direction",
+//       direction === "left" ? "forwards" : "reverse"
+//     );
+//   };
+//   const getSpeed = () => {
+//     const duration = speed === "fast" ? "20s" : speed === "normal" ? "40s" : "80s";
+//     containerRef.current.style.setProperty("--animation-duration", duration);
+//   };
+//   return (
+//     <div
+//       ref={containerRef}
+//       className={cn(
+//         "scroller relative z-20 max-w-7xl overflow-hidden [mask-image:linear-gradient(to_right,transparent,white_20%,white_80%,transparent)]",
+//         className
+//       )}
+//     >
+//       <ul
+//         ref={scrollerRef}
+//         className={cn(
+//           "flex min-w-full shrink-0 gap-4 py-4 w-max flex-nowrap",
+//           start && "animate-scroll",
+//           pauseOnHover && "hover:[animation-play-state:paused]"
+//         )}
+//       >
+//         {items.map((item, idx) => (
+//           <li
+//             className="w-[700px] max-w-full relative h-128 top-20 rounded-2xl border border-b-0 flex-shrink-0 border-slate-700 px-8 py-6 md:w-[750px]"
+//             style={{ background: "linear-gradient(180deg, var(--slate-800), var(--slate-900)" }}
+//             key={idx}
+//           >
+//             {/* Dynamic component rendering based on item type */}
+//             {item.type === 'donut' && <DonutChart data={item.data} xName={item.xName} yName={item.yName} />}
+//             {item.type === 'area' && <AreaChart data={item.data} xName={item.xName} yName={item.yName} />}
+//           </li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// };
+
+// export default InfiniteMovingCards;
 
 import { cn } from "../utils/cn";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef } from "react";
+import DonutChart from './DonutChart';
+import AreaChart from './AreaChart';
 
-export const InfiniteMovingCards = ({
+type Item = {
+  type: string;
+  data: any[];
+  xName: string;
+  yName: string;
+  className?: string;  // Making className optional
+};
+
+type InfiniteMovingCardsProps = {
+  items: Item[];
+  direction?: "left" | "right";
+  speed?: "fast" | "normal" | "slow";
+  pauseOnHover?: boolean;
+  className?: string;
+};
+
+export const InfiniteMovingCards: React.FC<InfiniteMovingCardsProps> = ({
   items,
   direction = "left",
   speed = "fast",
   pauseOnHover = true,
   className,
-}: {
-  items: {
-    quote: string;
-    name: string;
-    title: string;
-  }[];
-  direction?: "left" | "right";
-  speed?: "fast" | "normal" | "slow";
-  pauseOnHover?: boolean;
-  className?: string;
 }) => {
-  const containerRef = React.useRef<HTMLDivElement>(null);
-  const scrollerRef = React.useRef<HTMLUListElement>(null);
+  const containerRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
-    addAnimation();
-  }, []);
-  const [start, setStart] = useState(false);
-  function addAnimation() {
-    if (containerRef.current && scrollerRef.current) {
-      const scrollerContent = Array.from(scrollerRef.current.children);
+    const scrollContainer = containerRef.current;
+    if (!scrollContainer) return;
 
-      scrollerContent.forEach((item) => {
-        const duplicatedItem = item.cloneNode(true);
-        if (scrollerRef.current) {
-          scrollerRef.current.appendChild(duplicatedItem);
-        }
-      });
+    let animationFrameId;
 
-      getDirection();
-      getSpeed();
-      setStart(true);
-    }
-  }
-  const getDirection = () => {
-    if (containerRef.current) {
+    const scrollEffect = () => {
       if (direction === "left") {
-        containerRef.current.style.setProperty(
-          "--animation-direction",
-          "forwards"
-        );
+        scrollContainer.scrollLeft += 1;
       } else {
-        containerRef.current.style.setProperty(
-          "--animation-direction",
-          "reverse"
-        );
+        scrollContainer.scrollLeft -= 1;
       }
-    }
-  };
-  const getSpeed = () => {
-    if (containerRef.current) {
-      if (speed === "fast") {
-        containerRef.current.style.setProperty("--animation-duration", "20s");
-      } else if (speed === "normal") {
-        containerRef.current.style.setProperty("--animation-duration", "40s");
-      } else {
-        containerRef.current.style.setProperty("--animation-duration", "80s");
+
+      // Reset scroll position
+      if (direction === "left" && scrollContainer.scrollWidth - scrollContainer.scrollLeft <= scrollContainer.clientWidth) {
+        scrollContainer.scrollLeft = 0;
+      } else if (direction === "right" && scrollContainer.scrollLeft <= 0) {
+        scrollContainer.scrollLeft = scrollContainer.scrollWidth - scrollContainer.clientWidth;
       }
+
+      animationFrameId = requestAnimationFrame(scrollEffect);
+    };
+
+    const startScrolling = () => animationFrameId = requestAnimationFrame(scrollEffect);
+    const stopScrolling = () => cancelAnimationFrame(animationFrameId);
+
+    startScrolling();
+
+    // Optional: Pause on hover
+    if (pauseOnHover) {
+      scrollContainer.addEventListener('mouseenter', stopScrolling);
+      scrollContainer.addEventListener('mouseleave', startScrolling);
     }
-  };
+
+    return () => {
+      cancelAnimationFrame(animationFrameId);
+      if (pauseOnHover) {
+        scrollContainer.removeEventListener('mouseenter', stopScrolling);
+        scrollContainer.removeEventListener('mouseleave', startScrolling);
+      }
+    };
+  }, [direction, pauseOnHover]);
+
   return (
     <div
       ref={containerRef}
       className={cn(
-        "scroller relative z-20  max-w-7xl overflow-hidden  [mask-image:linear-gradient(to_right,transparent,white_20%,white_80%,transparent)]",
+        "scroller relative z-20 max-w-7xl overflow-hidden [mask-image:linear-gradient(to_right,transparent,white_20%,white_80%,transparent)]",
         className
       )}
     >
       <ul
-        ref={scrollerRef}
         className={cn(
-          " flex min-w-full shrink-0 gap-4 py-4 w-max flex-nowrap",
-          start && "animate-scroll ",
+          "flex min-w-[200%] shrink-0 gap-4 py-4 w-max flex-nowrap",
+          "animate-scroll",
           pauseOnHover && "hover:[animation-play-state:paused]"
         )}
       >
-        {items.map((item, idx) => (
+        {items.concat(items).map((item, idx) => (
           <li
-            className="w-[350px] max-w-full relative h-64 rounded-2xl border border-b-0 flex-shrink-0 border-slate-700 px-8 py-6 md:w-[450px]"
-            style={{
-              background:
-                "linear-gradient(180deg, var(--slate-800), var(--slate-900)",
-            }}
-            key={item.name}
+            className="w-[700px] max-w-full relative min-h-[32rem] top-20 rounded-2xl border border-b-0 flex-shrink-0 border-slate-700 px-8 py-6 md:w-[750px]"
+            style={{ background: "linear-gradient(180deg, var(--slate-800), var(--slate-900)" }}
+            key={idx}
           >
-            <blockquote>
-              <div
-                aria-hidden="true"
-                className="user-select-none -z-1 pointer-events-none absolute -left-0.5 -top-0.5 h-[calc(100%_+_4px)] w-[calc(100%_+_4px)]"
-              ></div>
-              <span className=" relative z-20 text-sm leading-[1.6] text-gray-100 font-normal">
-                {item.quote}
-              </span>
-              <div className="relative z-20 mt-6 flex flex-row items-center">
-                <span className="flex flex-col gap-1">
-                  <span className=" text-sm leading-[1.6] text-gray-400 font-normal">
-                    {item.name}
-                  </span>
-                  <span className=" text-sm leading-[1.6] text-gray-400 font-normal">
-                    {item.title}
-                  </span>
-                </span>
-              </div>
-            </blockquote>
+            {item.type === 'donut' && <DonutChart data={item.data} xName={item.xName} yName={item.yName} />}
+            {item.type === 'area' && <AreaChart data={item.data} xName={item.xName} yName={item.yName} />}
           </li>
         ))}
       </ul>
@@ -120,3 +306,4 @@ export const InfiniteMovingCards = ({
   );
 };
 
+export default InfiniteMovingCards;
